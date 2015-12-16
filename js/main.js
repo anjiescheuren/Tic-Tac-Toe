be16541da87fd3e8324e70a7128fc8e10ca1e282
Anjie Scheuren <emailanjie@gmail.com>
2015-12-16 14:55:07 -0600

    Board is responsive! JS is pseudocoded

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//document ready function
+$(function () {
+
+  // create 8 different variables equal to arrays for each win condition
+
+  // function for User1 to click on a button in the grid
+    // mark an 'X' (.click)
+
+  // function for User2 to click on a button in the grid
+    // mark an 'O' (.click)
+
+  // loop through 8 arrays
+    // if there are 3 Xs or 3 Os in an array
+      // alert "X wins!" or "O wins!".
+    // else if there are not 3 Xs or 3 Os in an array
+      // alert "Tie!".
+
+  // clear board function
+
+});
