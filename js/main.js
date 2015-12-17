556c31cd76d4ca4b5b40a89c06b436c450d13b5d
Anjie Scheuren <emailanjie@gmail.com>
2015-12-17 13:55:57 -0600

    added function to click buttons only once

--- js/main.js
+++ js/main.js
@@ -15,138 +15,134 @@ $(function() {
         buttons.push(this.id);
     });
 
-    $('#a').click(function() {
+    // create a function to wrap click functions
+    function buttonClick() {
+
+      $('#a').click(function() {
+          if (clickCount % 2 === 0) {
+              var shape = "<img src='images/X.png'>";
+          } else {
+              shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          // onclick, addClass to button to only allow one click
+          $(this).addClass('occupied');
+          // create a conditional that only lets you click on a button once
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
+
+      $('#b').click(function() {
+          if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+              shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
+
+      $('#c').click(function() {
         if (clickCount % 2 === 0) {
             var shape = "<img src='images/X.png'>";
-        } else {
+          } else {
             shape = "<img src='images/O.png'>";
-        }
+          }
         $(this).html(shape);
-        // onclick, addClass to button to only allow one click
         $(this).addClass('occupied');
-        // create a conditional that only lets you click on a button once
         if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+            $(this).off();
+          }
+      });
 
-    $('#b').click(function() {
+      $('#d').click(function() {
         if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
+            var shape = "<img src='images/X.png'>";
+          } else {
             shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
 
-    $('#c').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-      $(this).html(shape);
-      $(this).addClass('occupied');
-      if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+      $('#e').click(function() {
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+            shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
 
-    $('#d').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+      $('#f').click(function() {
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+            shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
 
-    $('#e').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+      $('#g').click(function() {
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+            shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
 
-    $('#f').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
+      $('#h').click(function() {
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+            shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
 
-    $('#g').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
-
-    $('#h').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
-
-    $('#j').click(function() {
-      if (clickCount % 2 === 0) {
-          var shape = "<img src='images/X.png'>";
-        } else {
-          shape = "<img src='images/O.png'>";
-        }
-        $(this).html(shape);
-        $(this).addClass('occupied');
-        if ($('div').hasClass('occupied')) {
-          $(this).off();
-        }
-    });
-});
+      $('#j').click(function() {
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+          } else {
+            shape = "<img src='images/O.png'>";
+          }
+          $(this).html(shape);
+          $(this).addClass('occupied');
+          if ($('div').hasClass('occupied')) {
+            $(this).off();
+          }
+      });
+    }
+  buttonClick();
 
 // get the value for a position on the board
 // var a = $('#a').val();
 
 // create 8 different variables equal to arrays for each win condition
-
-// function for User1 to click on a button in the grid
-// mark an 'X' (.click)
-
-
-
-
-
 
 // loop through 8 arrays
 // if there are 3 Xs or 3 Os in an array
@@ -156,10 +152,8 @@ $(function() {
 
 // clear board function
 $('.clear').click(function() {
-    $('.btn').empty();
-    if ($('div').hasClass('.btn-xl')) {
-
-    }
+    $('.btn-xl').empty();
+    buttonClick();
 });
 
-// });
+});
