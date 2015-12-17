d628eddd85de523a8e89ffd7cd33119f46c6b88b
Anjie Scheuren <emailanjie@gmail.com>
2015-12-16 22:17:26 -0600

    alternating exes and ohs work

--- js/main.js
+++ js/main.js
@@ -1,88 +1,129 @@
 //document ready function
-$(function () {
+$(function() {
 
-  // assign X a number (1)
-  var X = 1;
-  // assign O a number (9)
-  var O = 9;
-  // create an array for all the spots on the board
-  var left = [];
-  var middle = [];
-  var right = [];
+    // create a variable that counts clicks
+    var val = 0;
+    var clickCount = 0;
+    $('.btn-xl').click(function() {
+        clickCount = (val++);
+    });
 
-  // get the value for a position on the board
-  // var a = $('#a').val();
+    // create an array for all the spots on the board
+    var buttons = [];
+    $(".board").find(".btn").each(function() {
+        buttons.push(this.id);
+    });
 
-  // create 8 different variables equal to arrays for each win condition
+    // loop through the array to assign X to each button
+    // alternate player clicks. X clicks first. (modulus)
+    // for (var i = 0; i < buttons.length; i++) {
+    // buttons[i]
 
-  // function for User1 to click on a button in the grid
-    // mark an 'X' (.click)
 
     $('#a').click(function() {
-      $(this).text('X');
-      console.log('picture');
+        if (clickCount % 2 === 0) {
+            var shape = "<img src='images/X.png'>";
+        } else {
+            shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#b').click(function() {
-      $(this).text('X');
-      console.log('picture');
+        if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+            shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#c').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+      $(this).html(shape);
     });
 
     $('#d').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#e').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#f').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#g').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#h').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
 
     $('#j').click(function() {
-      $(this).text('X');
-      console.log('picture');
+      if (clickCount % 2 === 0) {
+          var shape = "<img src='images/X.png'>";
+        } else {
+          shape = "<img src='images/O.png'>";
+        }
+        $(this).html(shape);
     });
-
-  // alternate player clicks. X clicks first. (modulus)
-
-  // function for User2 to click on a button in the grid
-    // mark an 'O' (.click)
-    $('#b').click(function() {
-      $(this).text('O');
-      console.log('picture');
-    });
-
-  // loop through 8 arrays
-    // if there are 3 Xs or 3 Os in an array
-      // alert "X wins!" or "O wins!".
-    // else if there are not 3 Xs or 3 Os in an array
-      // alert "Tie!".
-
-  // clear board function
-  $('.clear').click(function() {
-    $('.btn').empty();
-  });
-
 });
 
+// get the value for a position on the board
+// var a = $('#a').val();
 
+// create 8 different variables equal to arrays for each win condition
+
+// function for User1 to click on a button in the grid
+// mark an 'X' (.click)
+
+
+
+
+
+
+// loop through 8 arrays
+// if there are 3 Xs or 3 Os in an array
+// alert "X wins!" or "O wins!".
+// else if there are not 3 Xs or 3 Os in an array
+// alert "Tie!".
+
+// clear board function
+$('.clear').click(function() {
+    $('.btn').empty();
+});
+
+// });
